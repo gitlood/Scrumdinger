eba89058058a8ed072ebce7741422d4af0b1bcda
chall <christopher.hall@trademe.co.nz>
2023-08-09 10:59:48 +1200

    Card is displaying scrum information

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by chall on 9/08/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
