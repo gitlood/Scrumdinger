9f140b928fd37aaefe180478e397d928d40b46a1
chall <christopher.hall@trademe.co.nz>
2023-08-09 11:27:40 +1200

    accessibility added to card view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -13,11 +13,14 @@ struct CardView: View {
         VStack (alignment: .leading){
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes meeting")
                     .labelStyle(.trailingIcon)
             }
             .font(.caption)
