54ff4f818678454b453a5a2e69a3df00a314a33a
chall <christopher.hall@trademe.co.nz>
2023-08-10 13:49:30 +1200

    Updated padding and assists

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -8,22 +8,22 @@
 import SwiftUI
 
 enum Theme: String {
-case bubblegum
-case buttercup
-case indigo
-case lavender
-case magenta
-case navy
-case orange
-case oxblood
-case periwinkle
-case poppy
-case purple
-case seafoam
-case sky
-case tan
-case teal
-case yellow
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
     
     var accentColor: Color {
         switch self {
