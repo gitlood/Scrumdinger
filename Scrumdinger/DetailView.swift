8b5562674573cd0600bc769467f03516d23eb2a5
chall <christopher.hall@trademe.co.nz>
2023-08-15 08:51:40 +1200

    Create detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by chall on 15/08/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -13,7 +13,7 @@ struct ScrumsView: View {
     var body: some View {
         NavigationStack{
             List(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)){
+                NavigationLink(destination: DetailView(scrum: scrum)){
                     CardView(scrum: scrum)
                 }
                 .listRowBackground(scrum.theme.mainColor)
