8696e81fae68a8bd8f58b66e997c61fde34f4965
chall <christopher.hall@trademe.co.nz>
2023-08-28 08:52:29 +1200

    Add accessibility modifiers

--- Scrumdinger/UI/DetailScreen/DetailEditView.swift
+++ Scrumdinger/UI/DetailScreen/DetailEditView.swift
@@ -19,8 +19,10 @@ struct DetailEditView: View {
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1){
                         Text("Length")
                     }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
                 }
             }
             Section(header: Text("Attendees")){
@@ -40,6 +42,7 @@ struct DetailEditView: View {
                         }
                     }){
                         Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
